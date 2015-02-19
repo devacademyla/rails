6f74a85d9a206705580997f4f0a7d69603cedcf2
Kattya Cuevas <kattya.cuevas@gmail.com>
2015-02-19 13:06:40 -0500

    ejercicio: devflix

---
+++ 1_fundamentos/asesoria/devflix/spec/controllers/series_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe SeriesController, type: :controller do
+  describe 'GET #index' do
+    it 'retorna http success' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'retorna 1 serie' do
+      serie = Serie.create(nombre: 'Bates Motel')
+      get :index
+      expect(assigns(:series).size).to eq 1
+      expect(assigns(:series).first).to eq serie
+    end
+  end
+end
