2f5f109b0bddd910bbf7b4900876c5fd86e30c31
Kattya Cuevas <kattya.cuevas@gmail.com>
2015-02-27 14:34:59 -0500

    Correr migracion

--- 1_fundamentos/clase/crud_producto/demo/db/schema.rb
+++ 1_fundamentos/clase/crud_producto/demo/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227192154) do
+ActiveRecord::Schema.define(version: 20150227193334) do
 
   create_table "productos", force: :cascade do |t|
     t.string   "titulo"
